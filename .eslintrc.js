826d399280d73fcd552aa5b08d15f5a784543ff6
yaotaiyang <yaotaiyang99@gmail.com>
2020-03-14 23:15:55 +0800

    axis bug fixed

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    //设置"script"（默认）或"module"如果你的代码是在ECMAScript中的模块。
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  globals: {},
+  // add your custom rules here
+  rules: {
+    'newline-per-chained-call': 'off'
+  }
+}
