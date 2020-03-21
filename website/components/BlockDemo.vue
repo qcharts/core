69805b314fbb8e11a312775e72f48e94f9bbd871
yaotaiyang <yaotaiyang99@gmail.com>
2020-03-21 20:58:22 +0800

    修改打包脚本、文档目录等

--- website/components/BlockDemo.vue
+++ website/components/BlockDemo.vue
@@ -111,7 +111,7 @@ export default {
       let qchartsSrc =
         process.env.NODE_ENV === 'development'
           ? location.origin + '/qcharts.js'
-          : 'https://s4.ssl.qhres.com/static/eee4270b9365f11e.js'
+          : './qcharts.js'
       const spritejsSrc =
         process.env.NODE_ENV === 'development'
           ? 'https://unpkg.com/spritejs@3/dist/spritejs.min.js'
