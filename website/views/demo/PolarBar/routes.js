30d4d0b78da8f03256caaf7163bf48fe1c4cc01d
longwind <287560869@qq.com>
2020-07-16 14:50:59 +0800

    update Nightingale rose name

--- website/views/demo/PolarBar/routes.js
+++ website/views/demo/PolarBar/routes.js
@@ -1,28 +1,28 @@
 export default [
   {
-    title: '',
-    path: '',
-    redirect: 'default'
+    title: "",
+    path: "",
+    redirect: "default",
   },
 
   {
-    title: '基础柱状图',
-    path: 'default',
-    component: () => import('./default-polarBar.md')
+    title: "基础图",
+    path: "default",
+    component: () => import("./default-polarBar.md"),
   },
   {
-    title: '分组柱状图',
-    path: 'group',
-    component: () => import('./group-polarBar.md')
+    title: "分组图",
+    path: "group",
+    component: () => import("./group-polarBar.md"),
   },
   {
-    title: '堆叠柱状图',
-    path: 'stack',
-    component: () => import('./stack-polarBar.md')
+    title: "堆叠图",
+    path: "stack",
+    component: () => import("./stack-polarBar.md"),
   },
   {
-    title: '其他',
-    path: 'others',
-    component: () => import('./others-polarBar.md')
-  }
-]
+    title: "其他",
+    path: "others",
+    component: () => import("./others-polarBar.md"),
+  },
+];
