ab2382cbcfe29c252b8a2bf4946a51f099dc4cf4
yaotaiyang <yaotaiyang99@gmail.com>
2020-07-26 11:18:43 +0800

    update symbol

--- src/utils/Symbol.js
+++ src/utils/Symbol.js
@@ -1,6 +1,6 @@
-import { Sprite, Path, Ellipse, Rect, Triangle, Parallel, Regular, Star, Node } from 'spritejs'
+import { Sprite, Path, Ellipse, Rect, Triangle, Parallel, Regular, Star, Group } from 'spritejs'
 import { jsType } from '@qcharts/utils'
-// import Base from '../base/Base'
+import filterClone from 'filter-clone'
 const allSymbol = {
   sprite: Sprite,
   path: Path,
@@ -11,33 +11,46 @@ const allSymbol = {
   regular: Regular,
   star: Star
 }
-class Symbol {
-  constructor(attrs) {
-    this.theme = {}
+class Symbol extends Group {
+  constructor(attrs = {}) {
+    super()
     this._attrs = attrs
     if (attrs && attrs.pointType) {
       this.TagName = allSymbol[attrs.pointType]
     } else {
       this.TagName = Ellipse
     }
-    this.$sprite = new this.TagName(attrs)
-    return this.$sprite
+    let renderAttr = filterClone(attrs, [], ['pos'])
+    this.$sprite = new this.TagName(renderAttr)
+    this.append(this.$sprite)
+    if (attrs.pos && attrs.pos.length) {
+      this.attr('pos', pos)
+    }
   }
   attr(name, val) {
     //属性设置
-    let _name = name
-    let _val = val
-    if (jsType(name) === 'object' && name[pointType] !== this._attrs.pointType) {
-      _name = 'pointType'
-      _val = name.pointType
+    if (jsType(name) === 'object') {
+      if (name.pointType) {
+        // 如果存在pointType，优先进行替换操作，否则会有属性赋值不上
+        this.attr('pointType', name.pointType)
+      }
+      for (let key in name) {
+        if (key !== 'pointType') {
+          this.attr(key, name[key])
+        }
+      }
     }
-    //替换dom
-    if (_name === 'pointType' && _val !== this._attrs.pointType) {
-      const $newSprite = new allSymbol[_val]()
-      this.$sprite.replaceWith($newSprite)
-      this.$sprite = $newSprite
+    // 如果是pos，设置为父级
+    if (name === 'pos') {
+      super.attr('pos', val)
+    } else if (name === 'pointType') {
+      // 如果存在替换操作
+      this.$sprite.remove()
+      this.$sprite = new allSymbol[val]()
+      this.append(this.$sprite)
+    } else {
+      this.$sprite.attr(name, val)
     }
-    this.$sprite.attr(name, val)
   }
 }
 export default Symbol
