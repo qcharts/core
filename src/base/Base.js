e29c929ff9172d622cd5ea194ad5eb979d7f0b05
longwind <287560869@qq.com>
2020-08-04 10:01:05 +0800

    修复dataset新建后丢失监听

--- src/base/Base.js
+++ src/base/Base.js
@@ -99,10 +99,10 @@ class Base extends Node {
     } else {
       store.dataset = data;
     }
-    store.dataset.on("change", (_) => {
-      this.__update({ type: "state" });
-    });
     if (store.dataset && store.__isCreated__) {
+      store.dataset.on("change", (_) => {
+        this.__update({ type: "state" });
+      });
       //如果以前存在，则更新
       this.__update({ type: "source" });
     }
