3d55c09ae2d93befeb9d3e948e47613d7f9e4012
yaotaiyang <yaotaiyang99@gmail.com>
2020-08-20 10:54:10 +0800

    add bar doc

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import { getGlobal } from './base/platform'
 import Chart from './Chart'
 import theme from './theme/default'
 const qcharts = {
-  version: '1.0.7',
+  version: '1.0.8',
   Line,
   Pie,
   Area,
