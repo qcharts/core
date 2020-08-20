d29cf17872283008bbad7155871f5955c1190bfb
zhouzhili <zhouzhili@360.cn>
2020-08-20 16:26:12 +0800

    chore:添加按需引入打包方式

--- babel.config.js
+++ babel.config.js
@@ -2,18 +2,34 @@
 
 module.exports = function(api) {
   const env = api.env()
-  return {
-    presets: [['@babel/preset-env']],
+
+  const getPragma = () => (env === 'wxapp' ? 'getApp().qcharts.h' : 'qcharts.h')
+
+  const setting = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          modules: false,
+          targets: {
+            browsers: ['> 1%', 'last 2 versions', 'not ie <= 9']
+          }
+        }
+      ]
+    ],
     plugins: [
       '@babel/plugin-proposal-class-properties', // jsx 中需要，删除报错
+      '@babel/plugin-proposal-object-rest-spread',
       [
         '@babel/plugin-transform-react-jsx',
         {
-          pragma: env === 'wxapp' ? 'getApp().qcharts.h' : 'qcharts.h'
+          pragma: getPragma()
         }
       ],
       'inline-package-json',
       ['@babel/plugin-transform-runtime']
     ]
   }
+
+  return setting
 }
