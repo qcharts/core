fb352a5f57616b4e3078c73f5280bfaa48a9504d
yaotaiyang <yaotaiyang99@gmail.com>
2020-08-21 11:21:19 +0800

    获取 constructor name处理

--- src/base/BasePlugin.js
+++ src/base/BasePlugin.js
@@ -5,10 +5,10 @@ import filterClone from 'filter-clone'
 export default class BasePlugin extends Base {
   constructor(attrs) {
     super(attrs)
-    this.theme = theme.plugins[this.constructor.name]
+    this.theme = theme.plugins[this.constructorName]
     //如果当前对象设置了theme，不继承theme
     this.theme.set = obj => {
-      if (this.theme === theme.visuals[this.constructor.name]) {
+      if (this.theme === theme.visuals[this.constructorName]) {
         this.theme = filterClone(this.theme)
       }
       this.theme = deepObjectMerge(this.theme, obj)
